d2de7d30d3029acf2748a467018267d11c0e8820
Alex <akjr820@gmail.com>
2025-07-08 10:00:31 -0400

    saved progress on laptop

--- oop-exercise.js
+++ oop-exercise.js
@@ -1,1 +1,43 @@
 /* Please read the instructions located in the exercises/2-oop-exercise-readme.md file and complete your code below */
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.grades = [];
+  }
+  addGrade(grade, ...rest) {
+    if (grade > 0 && grade < 100) {
+      this.grades.push(grade);
+    } else if (rest > 0 && rest < 100) {
+      this.grades.push(...rest);
+    } else {
+      console.log("Grade must be between 0 and 100");
+    }
+  }
+  calculateAverageGrade() {
+    if (this.grades !== undefined) {
+      averageGrade = this.grades.reduce(
+        (acc, val) => (acc + val) / this.grades.length
+      );
+    } else {
+      return 0;
+    }
+  }
+  hasPassed(passingGrade) {
+    return averageGrade > passingGrade;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+}
+
+const student1 = new Student("Alex", 25);
+student1.addGrade(80, 90, 70, 65, 89);
+console.log(student1);
+
+class Course {
+  constructor() {}
+}
